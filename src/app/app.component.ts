77bb1b32517714021c0f82e6f46f59cec9f374d0
jhonnoriega1221 <jhonlex17@gmail.com>
2023-03-06 15:10:15 -0500

    feat: se tienen los cimientos de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  name:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  public tasks:Task[] = [{name: 'ola'},{name: 'aa'}];
 }
