b7dbc6eb9f61c525d02eb3a451694c4d8aff6ed0
jhonnoriega1221 <jhonlex17@gmail.com>
2023-03-06 17:47:47 -0500

    feat: añadir funcionalidad de agregar tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Task } from './models/task';
 import { TaskService } from './services/task.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,32 @@ import { TaskService } from './services/task.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  @ViewChild('addTaskDialog') addTaskDialog: ElementRef | undefined;
   
   public tasks:Task[] = [];
+  public taskForm:FormGroup;
 
-  constructor(private _taskService:TaskService){}
+  constructor(private _taskService:TaskService, private fb:FormBuilder){
+    this.taskForm = this.fb.group({
+      taskNameInput: new FormControl('', [Validators.required, Validators.minLength(4)])
+    });
+  }
 
   ngOnInit(): void {
     this.tasks = this._taskService.getTasks();
+  }
+
+  public submitTask():void{
+    if(this.taskForm.invalid)
+      return;
+
+    const task:Task = {
+      name: this.taskForm.get('taskNameInput')?.value,
+    }
+
+    this._taskService.addTask(task);
+    this.addTaskDialog?.nativeElement.close();
+    name: this.taskForm.reset('taskNameInput');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
@@ -10,7 +11,8 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     TaskItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
